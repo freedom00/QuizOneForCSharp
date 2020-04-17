8672e420e1e13d7ac3be78f9655d9e2a5c698546
Zhaosheng Liu <jonathan888999@163.com>
2020-04-17 19:08:00 -0400

    Add project files.

---
+++ QuizOne/MyExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizOne
+{
+    public static class MyExtensionMethods
+    {
+        public static bool IsDividableByThree(this int number) => 0 == number % 3;
+    }
+}
---
+++ QuizOne/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizOne
+{
+    internal class Student
+    {
+        public event EventHandler StudentGraduated;
+
+        public string Name { get; set; }
+        public bool Isgraduated { get; set; }
+
+        private bool isGraduated;
+
+        public bool IsGraduated
+        {
+            get { return isGraduated; }
+            set
+            {
+                if (value)
+                {
+                    OnStudentGraduated(EventArgs.Empty);
+                }
+                isGraduated = value;
+            }
+        }
+
+        protected virtual void OnStudentGraduated(EventArgs e)
+        {
+            EventHandler handler = StudentGraduated;
+            handler?.Invoke(this, e);
+        }
+    }
+}
